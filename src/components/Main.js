59621b0043a06614219109a00a5cd35ed4a4f72e
admin <khatrivinay1@gmail.com>
2022-03-11 22:49:08 +0530

    first project

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+function Main()
+{
+    return (
+        <main>
+            <h1>Fun Fcts about React</h1>
+            <ul className="items">
+                <li>lorem 1</li>
+                <li>lorem 2 </li>
+                <li>lorem 3 </li>
+                <li>lorem 4</li>
+                <li>lorem 5</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import "../index.css"
+import logo from "../images/logo.svg"
+function Nav()
+{
+    return (
+        <nav>
+            <img src = {logo} alt="some random"/>
+            <h3>React Facts </h3>
+            <h4>React Course- Project 1</h4>
+        </nav>
+    )
+}
+
+export default Nav;
